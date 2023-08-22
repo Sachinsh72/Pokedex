b2604ce29d95eeba1ffe1a5862e9da84a3520a7f
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-08-22 16:03:05 +0530

    React Router

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
 import './App.css'
-import Pokedex from './Components/Pokedex/pokedex'
+// import Pokedex from './Components/Pokedex/pokedex'
+import CustomRoutes from './routes/CustomeRoutes'
+import { Link } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <Pokedex/>
-    </>
+    <div className='outer-pokedex'>
+      <h1>
+          <Link to="/">
+        Pokedex
+        </Link>
+      </h1>
+      <CustomRoutes/>
+    </div>
   )
 }
 
--- pokedex/src/Components/Pokedex/pokedex.jsx
+++ pokedex/src/Components/Pokedex/pokedex.jsx
@@ -4,7 +4,6 @@ import './Pokedex.css'
 function Pokedex(){
     return(
         <div className="pokedex-wrapper">
-            <h1>Pokedex</h1>
             <Search/>
             <PokemonList/>
         </div>
